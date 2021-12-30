497262e91d6e45f3c4bf09a9a2e467b60ca871cd
Parth <d18cl115@charusat.edu.in>
2021-12-30 19:37:54 +0530

    first commit

---
+++ includes/_footer.php
@@ -0,0 +1,99 @@
+
+    <!-- sw footer section start-->
+    <div class="sw_footer_main_wrapper sw_footer_index_wrapper">
+
+        <div class="circle_btm">
+            <a href="javascript:" id="return-to-top"><i class="fa fa-long-arrow-up"></i></a>
+        </div>
+        <div class="footer_wrapper">
+
+            <div class="container">
+
+                <div class="row">
+                    <div class="foter_padder">
+                        <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+                            <div class="wrapper_second_about">
+                                <div class="wrapper_first_image">
+                                    <a href="index-2.html"><img src="images/hd_logo.gif" class="img-responsive" alt="logo" /></a>
+                                </div>
+                                <div class="abotus_content">
+                                    <p>We are here always here to serve you maintanace and any kind of technical support of your solar power system</p>
+                                    <ul>
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="col-lg-2 col-md-2 col-xs-12 col-sm-4">
+                            <div class="wrapper_second_useful">
+                                <h4>useful links </h4>
+                                <img src="images/hd_line_dark.png" alt="title" class="pd_btm_3">
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>About us</a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>events </a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>contact </a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>history</a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>news</a> </li>
+                                </ul>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-xs-12 col-sm-4">
+                            <div class="wrapper_second_useful wrapper_second_links">
+
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>sitemap</a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>FAQ </a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>awards </a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>tstimonials</a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i>career</a> </li>
+                                </ul>
+
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+                            <div class="wrapper_second_useful wrapper_second_useful_2">
+                                <h4>contact  us</h4>
+                                <img src="images/hd_line_dark.png" alt="title" class="pd_btm_3">
+                                <ul>
+									<li>
+                                     <h1>+919997865912</h1></li>
+									 <li><a href="#"><i class="flaticon-new-email-outline"></i>info@upkeepsolar.com</a>
+                                    </li>
+									 <li><a href="#"><i class="flaticon-internet"></i>www.upkeepsolar.com</a>
+                                    </li>
+                                  
+                                    <li><i class="flaticon-placeholder"></i>Ahmedabad, India
+                                    </li>                                   
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                    <div class="section3_copyright">
+                        <p>Copyright 2021-22 <a href="index-2.html"> Upkeep Solar Solutions</a>. all right reserved - design by <a href="webstrot.html">Parth Vaghasiya</a></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- sw footer section end-->
+    
